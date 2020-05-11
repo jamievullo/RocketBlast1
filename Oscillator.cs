f0feaeea976953b7528873791a32cd1e51f0be74
Jamie vullo <jvullo@protonmail.com>
2020-05-11 16:38:15 -0400

    Added movement of an object to make game more challenging and fun.

--- Oscillator.cs
+++ Oscillator.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
-
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
     [Range(0, 1)][SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved
 
     Vector3 startingPos; // must be stored for absolute movement
@@ -18,7 +18,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; //grows continually from zero
+
+        const float tau = Mathf.PI * 2; //+/- 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); //goes from -1 to +1
+
+        print(rawSinWave);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
     }
 }
+
