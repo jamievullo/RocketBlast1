15b4482625739d856f5b605ee6d954d472858e09
Jamie vullo <jvullo@protonmail.com>
2020-05-11 13:32:23 -0400

    Added new audio for rocket thrust and refactored code.

--- Rocket.cs
+++ Rocket.cs
@@ -8,6 +8,7 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -23,31 +24,41 @@ public class Rocket : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { // need to stop sound on death todo
+        if (state == State.Alive) 
+        {
         Thrust();
         Rotate();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-            break;
+                break;
             case "Finish":
-            state = State.Transcending;
-            Invoke("LoadNextScene", 1f); //parameterize time
-            break;
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f); //parameterize time
+                break;
             default:
-            print("Dead");
-            SceneManager.LoadScene(0);
-            break;
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f); //parameterize time
+                break;
         }
+        
     }
 
-    private void LoadNextScene() 
+    private void LoadNextLevel() 
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel() 
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Thrust()
