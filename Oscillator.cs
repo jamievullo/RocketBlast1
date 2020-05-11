c76afd81e955f003305d95cdf5d617c9c33d92d9
Jamie vullo <jvullo@protonmail.com>
2020-05-11 16:18:36 -0400

    Added Oscillator file for movement of obstacles.

---
+++ Oscillator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+
+    [Range(0, 1)][SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPos; // must be stored for absolute movement
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
